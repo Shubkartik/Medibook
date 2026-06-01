896ff3762f1f077487d74197863fda5c0e925d64
Shubham Thamke <168270483+Shubkartik@users.noreply.github.com>
2026-06-01 14:56:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Creating superuser..."
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@medibook.com', 'Admin@2024')" | python manage.py shell || true
+
 echo "Build complete!"
